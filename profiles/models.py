4ed3e60ff83d26b854fa0949faed3e46507eb3e0
Roxana Analia <roxanaameijide81@gmail.com>
2024-11-09 17:34:36 +0000

    Update allauth templates for the profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
@@ -19,6 +20,7 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, null=True, blank=True)
     def __str__(self):
         return self.user.username
+        
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
