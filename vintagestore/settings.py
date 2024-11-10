61ae22b8a0d7394bd37207c65463fc75ae58e698
Roxana Analia <roxanaameijide81@gmail.com>
2024-11-10 14:19:38 +0000

    Heroku deployment

--- vintagestore/settings.py
+++ vintagestore/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-roxana1981-vintagestore-t1f8e4vbbsl.ws.codeinstitute-ide.net','vintagestore-9c006be944c1.herokuapp.com', 'localhost']
 
