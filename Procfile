13d06e606e198a114b666f8f2d83a07fd2189b23
Roxana Analia <roxanaameijide81@gmail.com>
2024-11-10 10:04:54 +0000

    Initial test deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vintagestore.wsgi:application
--- vintagestore/settings.py
+++ vintagestore/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from pathlib import Path
-if os.path.isfile("env.py"):
-    import env
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +27,7 @@ SECRET_KEY = 'django-insecure-7y#z77cm=yl&ie1aa-9l$2w_fah8^jw-%vmq!*+wry#acnfr5x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-roxana1981-vintagestore-t1f8e4vbbsl.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-roxana1981-vintagestore-t1f8e4vbbsl.ws.codeinstitute-ide.net', 'localhost']
 
 
 # Application definition
@@ -118,12 +116,17 @@ WSGI_APPLICATION = 'vintagestore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
